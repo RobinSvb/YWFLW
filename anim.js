6c9a14e6eb0a24da693e4ca71ae10a6d695c8d3d
robinvasquez <robinsamuel2010@gmail.com>
2024-09-09 22:22:08 -0600

    nuev aletra

--- anim.js
+++ anim.js
@@ -4,8 +4,11 @@ var lyrics = document.querySelector("#lyrics");
 
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 var lyricsData = [
-  { text: "At the time", time: 15 },
-  { text: "The whisper of birds", time: 18 },
+  //{ text: "At the time", time: 15 },
+  //{ text: "The whisper of birds", time: 18 },
+
+  { text: "Eres algo inalcanzable", time: 15 },
+  { text: "Ya lo sé, pero no entiendo", time: 20 },
   { text: "Lonely before the sun cried", time: 27 },
   { text: "Fell from the sky", time: 32 },
   { text: "Like water drops", time: 33 },
